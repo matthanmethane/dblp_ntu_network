25514942151af8ae7036c2cddeecb2273d70e172
Ziyang Deng <56111086+ZYDeng91@users.noreply.github.com>
2021-04-13 00:16:35 +0800

    Update sample-q12.py

--- sample-q12.py
+++ sample-q12.py
@@ -49,47 +49,47 @@ def get_properties(G):
     number_of_edges=G.number_of_edges()
     average_degree=number_of_edges/number_of_nodes
     average_clustering=nx.average_clustering(G)
-    print("number of nodes: n =", number_of_nodes)
-    print("number of edges: ∑k =", number_of_edges)
-    print("average degree: <k> =", average_degree)
-    print("average clustering coefficient: <C> =", average_clustering)
-    print("diameter: d = ", end="")
+    #print("number of nodes: n =", number_of_nodes)
+    #print("number of edges: ∑k =", number_of_edges)
+    #print("average degree: <k> =", average_degree)
+    #print("average clustering coefficient: <C> =", average_clustering)
+    #print("diameter: d = ", end="")
     try: 
         diameter=nx.diameter(G)
-        print(diameter)
+        #print(diameter)
     except (nx.exception.NetworkXError):
         diameter = "NA"
-        print("The graph is not connected")
-    print("average distance: <d> = ", end="")
+        #print("The graph is not connected")
+    #print("average distance: <d> = ", end="")
     try: 
         average_distance = nx.average_shortest_path_length(G)
-        print(average_distance)
+        #print(average_distance)
     except (nx.exception.NetworkXError):
         average_distance = "NA"
-        print("The graph is not connected")
+        #print("The graph is not connected")
 
         
     degree_centrality = nx.degree_centrality(G)
     highest_degree_centrality_pid = max(degree_centrality, key=degree_centrality.get)
     highest_degree_centrality_value = degree_centrality.get(highest_degree_centrality_pid)
-    print("highest degree centrality:", highest_degree_centrality_pid, highest_degree_centrality_value)
+    #print("highest degree centrality:", highest_degree_centrality_pid, highest_degree_centrality_value)
     
     #default using max_iter = 100, tolerance=10^-6
     #eigen_centrality = nx.eigenvector_centrality_numpy(G)
     eigen_centrality = nx.eigenvector_centrality(G)
     highest_eigen_centrality_pid = max(eigen_centrality, key=eigen_centrality.get)
     highest_eigen_centrality_value = eigen_centrality.get(highest_eigen_centrality_pid)
-    print("highest eigen centrality:", highest_eigen_centrality_pid, highest_eigen_centrality_value)
+    #print("highest eigen centrality:", highest_eigen_centrality_pid, highest_eigen_centrality_value)
     
     closeness_centrality = nx.closeness_centrality(G)
     highest_closeness_centrality_pid = max(closeness_centrality, key=closeness_centrality.get)
     highest_closeness_centrality_value = closeness_centrality.get(highest_closeness_centrality_pid)
-    print("highest closeness centrality:", highest_closeness_centrality_pid, highest_closeness_centrality_value)
+    #print("highest closeness centrality:", highest_closeness_centrality_pid, highest_closeness_centrality_value)
     
     betweenness_centrality = nx.betweenness_centrality(G)
     highest_betweenness_centrality_pid = max(betweenness_centrality, key=betweenness_centrality.get)
     highest_betweenness_centrality_value = betweenness_centrality.get(highest_betweenness_centrality_pid)
-    print("highest betweenness centrality:", highest_betweenness_centrality_pid, highest_betweenness_centrality_value)
+    #print("highest betweenness centrality:", highest_betweenness_centrality_pid, highest_betweenness_centrality_value)
     
     #return a list of network properties
     result = []
@@ -120,27 +120,26 @@ result_df = pd.DataFrame.from_dict(result_dict)
 #result_df.to_csv('result_yearly.csv', index=False)
 
                     
-#create columns showing difference by year (current_year[data]-last_year[data])        
-df=result_df
-N=1
-shift_range = N+1
-merging_keys = ['Year']
-lag_cols = ['nodes','edges','average_degree','average_clustering']
-for shift in range(1,shift_range):
-    df_shift = df[merging_keys + lag_cols].copy()
-    
-    # E.g. Year of 2000 becomes 2001, for shift = 1.
-    # So when this is merged with Year of 2001 in df, this will represent lag of 2001.
-    df_shift['Year'] = df_shift['Year'] + shift
-    foo = lambda x: '{}_lag_{}'.format(x, shift) if x in lag_cols else x
-    df_shift = df_shift.rename(columns=foo)
-    df = pd.merge(df, df_shift, on=merging_keys, how='left') #.fillna(0)
-    
-    bar = lambda x: '{}_diff_{}'.format(x, shift) if x in lag_cols else x
-    for i in lag_cols:
-        df[bar(i)]=df[i]-df[foo(i)]
-        df.drop(foo(i), axis=1, inplace=True)
-del df_shift
+#create columns showing difference by year (current_year[data]-last_year[data])
+def yearly_diff(df,N=1)
+    shift_range = N+1
+    merging_keys = ['Year']
+    lag_cols = ['nodes','edges','average_degree','average_clustering']
+    for shift in range(1,shift_range):
+        df_shift = df[merging_keys + lag_cols].copy()
 
+        # E.g. Year of 2000 becomes 2001, for shift = 1.
+        # So when this is merged with Year of 2001 in df, this will represent lag of 2001.
+        df_shift['Year'] = df_shift['Year'] + shift
+        foo = lambda x: '{}_lag_{}'.format(x, shift) if x in lag_cols else x
+        df_shift = df_shift.rename(columns=foo)
+        df = pd.merge(df, df_shift, on=merging_keys, how='left') #.fillna(0)
+
+        bar = lambda x: '{}_diff_{}'.format(x, shift) if x in lag_cols else x
+        for i in lag_cols:
+            df[bar(i)]=df[i]-df[foo(i)]
+            df.drop(foo(i), axis=1, inplace=True)
+    del df_shift
+df = yearly_diff(df,N=1)
 #df.head()
     
